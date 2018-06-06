71b3f965d94f522125b85fe01ed3077b1ec72143
keith66fuller <keith66fuller@gmail.com>
2018-06-06 15:40:35 -0500

    fixed typo in listener

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const http = require("http");
 
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE "html">");
+  response.write("<!DOCTYPE \"html\">");
   response.write("<html>");
   response.write("<head>");
   response.write("<title>Hello World Page</title>");
